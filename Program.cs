4d09a6842f53916c7968e0c6ecf37f2dbe09b5b1
Max <maxbillyeven2@gmail.com>
2026-04-20 20:05:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть свій вік!:");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть свій ріст:");
+        int height = int.Parse(Console.ReadLine());
+
+        if (age > 18 && height >160)
+        {
+            Console.WriteLine("Вхід дозволений");
+            }
+       else
+        {
+            Console.WriteLine("вхід заборонений");
+            }
+         }
+    }
